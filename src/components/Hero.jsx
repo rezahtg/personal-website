f2766f6a4e1947cc6ffee42f6a3e26412a969023
Reza Hutagaol <silentsound05@gmail.com>
2026-03-21 00:00:59 +0700

    fix photo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
           <div className="flex items-center gap-3">
             <figure className="img-box w-9 h-9 rounded-lg">
               <img
-                src="/images/avatar-2.webp"
+                src="/images/Headshot2.jpeg"
                 width={40}
                 height={40}
                 alt="Reza Reynaldo Hutagaol Potrait - Java Backend Developer"
@@ -48,7 +48,7 @@ const Hero = () => {
         <div className="hidden lg:block">
           <figure className="w-full max-w-[400px] ml-auto bg-gradient-to-t from-sky-400 via-25% via-sky-400/40 to-65% rounded-[60px] overflow-hidden">
             <img
-              src="/images/pict-3.webp"
+              src="/images/Headshot2.jpeg"
               width={656}
               height={800}
               alt="Reza Reynaldo Hutagaol - Java Backend Developer"
